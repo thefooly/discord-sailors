f6597d847b98bf78d244506797b3fca2dc3ff723
Andrea Gallidabino <andrea.gallidabino@usi.ch>
2015-12-02 13:36:37 +0100

    twitter command extended

--- commands/twitter/index.js
+++ commands/twitter/index.js
@@ -1,6 +1,7 @@
 var debug = require("debug")('discord-sailors')
 
 var Twitter = require('twitter');
+var Stream = require('user-stream');
 var config = require('../../config');
  
 var twitterClient = new Twitter({
@@ -20,8 +21,23 @@ var message = function(bot, message) {
   	case 'unfollow':
   		unfollow(bot, message, s[3])
   		break;
+  	case 'track':
+  		track(bot, message, s[3])
+  		break;
+  	case 'untrack':
+  		untrack(bot, message, s[3])
+  		break;
+  	case 'followlist':
+  		followList(bot, message)
+  		break;
+  	case 'tracklist':
+  		trackList(bot, message)
+  		break;
   	case 'last':
   		last(bot, message, s[3])
+  		break;
+  	case 'post':
+  		post(bot, message, s[3])
   		break;
   	default:
   		bot.reply(message, "Invalid input");
@@ -31,18 +47,67 @@ var message = function(bot, message) {
 
 var help = function() {
 	var m = "twitter:\n"
-	m += "\t\t -- twitter follow x:\tfollows the tweets stream of 'x'\n"
-	m += "\t\t -- twitter unfollow x:\tunfollows the tweets stream of 'x'\n"
-	m += "\t\t -- twitter last x:\tpost the last tweet of 'x'"
+	// m += "\t\t -- twitter follow x:\tfollows the tweets stream of 'x'\n"
+	// m += "\t\t -- twitter unfollow x:\tunfollows the tweets stream of 'x'\n"
+	m += "\t\t -- twitter track x:\ttracks the tweets stream containing the word 'x'\n"
+	m += "\t\t -- twitter untrack x:\tuntracks the tweets stream containting the word 'x'\n"
+	m += "\t\t -- twitter last x:\tpost the last tweet of 'x'\n"
+	m += "\t\t -- twitter post x:\tpost the last tweet containing 'x'\n"
+	m += "\t\t -- twitter followlist:\treturns the list of followed users"
+	m += "\t\t -- twitter tracklist:\treturns the list of tracked words"
 
 	return m
 }
 
-var following = {}
+var following = []
+var followingStreams = {}
+
+var tracking = []
+var trackingStreams = {}
 
 var follow = function(bot, message, twitterUser) {
 	if(twitterUser) {
-		bot.reply(message, "Not yet implemented");
+		var params = {
+			screen_name: twitterUser
+		}
+		twitterClient.get('users/show', params, function(error, user, response){
+			if (!error) {
+		    if(user) {
+		    	var user_id = user.id_str
+
+		    	var stream_params = {
+		    		follow: '807095' 
+		    	}
+		    	var stream = createStream(stream_params)
+
+		    	stream.on('close', function() {
+		    		closeFollowStream(bot, message, twitterUser)
+		    	})
+
+		    	stream.on('error', function(error) {
+		    		debug('Error: twitter')
+		    		debug(error)
+		    		closeFollowStream(bot, message, twitterUser)
+		    	})
+
+		    	stream.on('data', function(data) {
+		    		if(data.text) {
+		    			bot.sendMessage(message.channel, 'Pimmi', {}, function(err, msg) {
+			    			if(err) {
+			    				debug('Error: twitter - failed to send message: ' + err)
+			    			}
+		    			})
+		    		}
+		    	})
+
+		    	followingStreams[twitterUser] = stream
+					following.push(twitterUser)
+		    	bot.reply(message, "Following " + twitterUser);
+		    }
+		  } else {
+		  	bot.reply(message, "Failed to retrieve user");
+		  }
+		})
 	} else {
 		bot.reply(message, "Username is not valid");
 	}
@@ -50,10 +115,62 @@ var follow = function(bot, message, twitterUser) {
 
 var unfollow = function(bot, message, twitterUser) {
 	if(twitterUser) {
-		bot.reply(message, "Not yet implemented");
+			closeFollowStream(bot, message, twitterUser)
 	} else {
 		bot.reply(message, "Username is not valid");
 	}
+}
+
+var track = function(bot, message, word) {
+	if(word) {
+		var stream_params = {
+			track: word 
+		}
+		
+		var stream = createStream(stream_params)
+
+  	stream.on('close', function() {
+  		closeTrackStream(bot, message, word)
+  	})
+
+  	stream.on('error', function(error) {
+  		debug('Error: twitter')
+  		debug(error)
+  		closeTrackStream(bot, message, word)
+  	})
+
+  	stream.on('data', function(data) {
+  		if(data.text) {
+  			bot.sendMessage(message.channel, data.text, {}, function(err, msg) {
+    			if(err) {
+    				debug('Error: twitter - failed to send message: ' + err)
+    			}
+  			})
+  		}
+  	})
+
+  	trackingStreams[word] = stream
+		tracking.push(word)
+  	bot.reply(message, "Tracking " + word);
+	} else {
+		bot.reply(message, "Input is not valid");
+	}
+}
+
+var untrack = function(bot, message, word) {
+	if(word) {
+			closeTrackStream(bot, message, word)
+	} else {
+		bot.reply(message, "Input is not valid");
+	}
+}
+
+var followList = function(bot, message) {
+	bot.reply(message, "Following: [" + following.join(', ') + "]" )
+}
+
+var trackList = function(bot, message) {
+	bot.reply(message, "Tracking: [" + tracking.join(', ') + "]" )
 }
 
 var last = function(bot, message, twitterUser) {
@@ -83,6 +200,80 @@ var last = function(bot, message, twitterUser) {
 		});
 	} else {
 		bot.reply(message, "Username is not valid");
+	}
+}
+
+var post = function(bot, message, word) {
+	if(word) {
+		var params = {
+			q: word,
+			result_type: 'mixed',
+			count: 1
+		};
+
+		twitterClient.get('search/tweets', params, function(error, search, response){
+		  if (!error) {
+		  	var tweets = search.statuses
+		    if(tweets.length > 0) {
+		    	var t = tweets[0]
+		    	var m = t.text + "\n\n"
+
+		    	if(t.entities.urls.url) {
+		    		m += "URL: " + t.entities.urls.url
+		    	}
+
+		    	bot.reply(message, m);
+		    } else {
+		    	bot.reply(message, "No tweets");
+		    }
+		  } else {
+		  	bot.reply(message, "Failed to retrieve tweet");
+		  }
+		});
+	} else {
+		bot.reply(message, "Input is not valid");
+	}
+}
+
+var createStream = function(params) {
+	var stream = new Stream({
+	  consumer_key: config.plugins.twitter.consumer_key,
+	  consumer_secret: config.plugins.twitter.consumer_secret,
+	  access_token_key: config.plugins.twitter.access_token_key,
+	  access_token_secret: config.plugins.twitter.access_token_secret
+	});
+
+	stream.stream(params)
+
+	return stream
+}
+
+var closeFollowStream = function(bot, message, user){
+	var pos = following.indexOf(user)
+	if(pos != -1) {
+		following.splice(pos, 1)
+		followingStreams[user].destroy()
+
+		bot.sendMessage(message.channel, 'Unfollowed user: ' + user, {}, function(err, msg) {
+			if(err) {
+				debug('Error: twitter - failed to send message: ' + err)
+			}
+		})
+	}
+}
+
+var closeTrackStream = function(bot, message, word){
+	var pos = tracking.indexOf(word)
+	if(pos != -1) {
+		tracking.splice(pos, 1)
+		trackingStreams[word].destroy()
+
+
+		bot.sendMessage(message.channel, 'Untracked: ' + word, {}, function(err, msg) {
+			if(err) {
+				debug('Error: twitter - failed to send message: ' + err)
+			}
+		})
 	}
 }
 
